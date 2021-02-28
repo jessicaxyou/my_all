b55a60bfb5ccd3897f32a52d28349091aecbc136
jessica you <jessicaxytan@gmail.com>
2021-02-27 22:32:04 -0500

    commit tree b569deeea61dbc1a5459f7293b784034a788bd9f

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,4 +2,22 @@ require 'pry'
 
 def my_all?(collection)
 
+    i = 0 #defining first element of the array
+
+    block_return_values = [] #creating new array to store yielded value
+
+    #iterates through the array using while and increasing i
+
+    while i < collection.length
+        block_return_values << yield(collection[i])
+         #this yields each elemnent of collection to block and store in new array
+        i = i + 1
+    end
+
+    if block_return_values.include?(false)
+        false
+    else
+        true
+    end
+
 end
